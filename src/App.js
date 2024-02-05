09a76e48a9a26065809c74fd9c6fea96e3d15be8
iwttairi <airiiwtt@gmail.com>
2024-02-05 13:01:23 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [result, setResult] = useState("");
+  const handleCalculation = (number) => {
+    const newResult = result + number;
+    setResult(newResult);
+  };
+  const handleReset = () => {
+    setResult("");
+  }
+  const execCalculation = () => {
+    try {
+      setResult(eval(result));
+    } catch (error) {
+      setResult("Error!");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dentaku!!!</h1>
+      <p className='string-box'>{result}</p>
+      <div className='column-box'>
+        <div className='row-box'>
+          <button className='normal' onClick={() => handleCalculation("7")}>7</button>
+          <button className='normal' onClick={() => handleCalculation("8")}>8</button>
+          <button className='normal' onClick={() => handleCalculation("9")}>9</button>
+          <button className='normal' onClick={() => handleCalculation("*")}>*</button>
+        </div>
+        <div className='row-box'>
+          <button className='normal' onClick={() => handleCalculation("4")}>4</button>
+          <button className='normal' onClick={() => handleCalculation("5")}>5</button>
+          <button className='normal' onClick={() => handleCalculation("6")}>6</button>
+          <button className='normal' onClick={() => handleCalculation("-")}>-</button>
+        </div>
+        <div className='row-box'>
+          <button className='normal' onClick={() => handleCalculation("1")}>1</button>
+          <button className='normal' onClick={() => handleCalculation("2")}>2</button>
+          <button className='normal' onClick={() => handleCalculation("3")}>3</button>
+          <button className='normal' onClick={() => handleCalculation("+")}>+</button>
+        </div>
+        <div className='row-box'>
+          <button className='normal' onClick={() => handleCalculation("0")}>0</button>
+          <button className='normal' onClick={() => handleCalculation(".")}>.</button>
+          <button className='normal' onClick={() => handleCalculation("/")}>/</button>
+          <button className='normal' onClick={() => execCalculation()}>=</button>
+        </div>
+        <div className='row-box'>
+          <button className='reset-button' onClick={() => handleReset()}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
